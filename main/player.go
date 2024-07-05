0bfa66ea117fce0e55f3a2d4a84dda40374e19a2
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-06 08:00:24 +1200

    Finished The Prototype
    - added gems to the game (blue, green, red and yellow)
    - added a Korean font, as this is a Korean language learning game
    - Added gui that displays a score and has a prompt for what item/colour the user is to collect
    - If you collect the item/colour the corresponds with the prompt then you get a point
    - If you collect the wrong item/colour then you lose a point
    - A sound is now played when you collect the correct item/colour and went you collect the incorrect one, too
    - The player is reset back to the start point after collecting a gem or falling off the platforms
    - when the player collects 10 gems the game is over and winning message appears
    - At the start of the game the two items/colours are randomised along with the prompt, too
    - After collecting a gem the items are also rerandomised along with the prompt
    - The player is not able to do anything/move once the game is over/they have won

--- main/player.go
+++ main/player.go
@@ -37,7 +37,7 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "collisionobject1"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
@@ -46,12 +46,13 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"wall\"\n"
+  "mask: \"gem\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: -4.0\n"
-  "      y: -119.0\n"
+  "      y: -96.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -65,7 +66,7 @@ embedded_components {
   "    id: \"\"\n"
   "  }\n"
   "  data: 32.0\n"
-  "  data: 9.0\n"
+  "  data: 32.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -88,7 +89,7 @@ embedded_components {
 embedded_components {
   id: "land"
   type: "sound"
-  data: "sound: \"/assets/player_sounds/footstep_wood_000.ogg\"\n"
+  data: "sound: \"/assets/sounds/footstep_wood_000.ogg\"\n"
   "looping: 0\n"
   "group: \"master\"\n"
   "gain: 1.0\n"
@@ -111,7 +112,76 @@ embedded_components {
 embedded_components {
   id: "jump"
   type: "sound"
-  data: "sound: \"/assets/player_sounds/phaseJump1.ogg\"\n"
+  data: "sound: \"/assets/sounds/phaseJump1.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "correct"
+  type: "sound"
+  data: "sound: \"/assets/sounds/powerUp7.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "incorrect"
+  type: "sound"
+  data: "sound: \"/assets/sounds/pepSound1.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "win"
+  type: "sound"
+  data: "sound: \"/assets/sounds/powerUp3.ogg\"\n"
   "looping: 0\n"
   "group: \"master\"\n"
   "gain: 1.0\n"
